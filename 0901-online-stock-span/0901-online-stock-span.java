d89f38d945c6438be90482d728ef07c595c8deb4
Sekhar-001 <veerasekhar005@gmail.com>
2026-02-24 10:18:04 +0530

    Time: 32 ms (43.22%), Space: 54.6 MB (44.94%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,59 +1,17 @@
-// Brute force
-
-// class StockSpanner {
-//     List<Integer> list;
-
-//     public StockSpanner() {
-//         list = new ArrayList<>();
-//     }
-    
-//     public int next(int price) {
-//         list.add(price);
-//         int count = 1;
-
-//         for(int i=list.size()-2; i>=0; i--) {
-//             if(list.get(i) <= price) count++;
-//             else break;
-//         }
-
-//         return count;
-//     }
-// }
-
-
-
-
-
-
-
-// Optimal Solution
-
-class StockSpanner {
-    Stack<int[]> st;
-    int idx;
-
-    public StockSpanner() {
-        st = new Stack<>();
-        idx = -1;
+import java.util.Stack;
+class StockSpanner{
+    Stack<int[]> stack;
+    public StockSpanner(){
+        stack=new Stack<>();
     }
+    public int next( int price){
+        int span=1;
     
-    public int next(int price) {
-        idx += 1;
-        
-        while(!st.isEmpty() && st.peek()[0] <= price) {
-            st.pop();
-        }
-        
-        int ans = idx - (st.isEmpty() ? -1 : st.peek()[1]);
-        st.push(new int[] {price, idx});
-
-        return ans;
-    }
+    while(!stack.isEmpty() && stack.peek()[0]<=price){
+        span+= stack.peek()[1];
+        stack.pop();
+       }
+    stack.push(new int[]{price,span});
+    return span;
+   }
 }
-
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner obj = new StockSpanner();
- * int param_1 = obj.next(price);
- */ 
